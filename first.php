d5c7a0c1b6d144b1b0f5d4a3fa1c66bdabb9e6c5
Yojjal Sharma <yojjal878@gmail.com>
2024-06-15 18:01:51 +0545

    concatenate string

--- first.php
+++ first.php
@@ -7,20 +7,10 @@
 </head>
 <body>
     <?php
-       class Car {
-        public $color;
-        public $model;
-        public function __construct($color, $model) {
-          $this->color = $color;
-          $this->model = $model;
-        }
-        public function message() {
-          return "My car is a " . $this->color . " " . $this->model . "!";
-        }
-      }
-      
-      $myCar = new Car("red", "Volvo");
-      var_dump($myCar);
+      $x = "Hello";
+      $y = "World";
+      $z = $x . $y;
+      echo $z;
     ?>
 </body>
 </html>
