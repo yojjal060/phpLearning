94cea1fec4a4a42a29b792e51a14f8069fb28865
Yojjal Sharma <yojjal878@gmail.com>
2024-06-15 17:30:24 +0545

    local variable doesn't work on outside function

--- first.php
+++ first.php
@@ -7,16 +7,14 @@
 </head>
 <body>
     <?php
-        $x = 5; // global scope
-
         function myTest() {
-          // using x inside this function will generate an error
-          $x = 5;
-          echo "<p>Variable x inside function is: $x</p>";
-        }
-        myTest();
-        
-        echo "<p>Variable x outside function is: $x</p>";
+            $x = 5; // local scope
+            echo "<p>Variable x inside function is: $x</p>";
+          }
+          myTest();
+          
+          // using x outside the function will generate an error
+          echo "<p>Variable x outside function is: $x</p>";
     ?>
 </body>
 </html>
